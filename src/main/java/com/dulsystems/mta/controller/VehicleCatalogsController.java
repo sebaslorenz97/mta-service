1a4188c4cfe7e846d026f3198a49de1fa7486974
David Uziel Lorenzo Sebastian <uziel.ls@refinitiv.com>
2024-06-03 15:18:42 -0600

    Modification for the VehicleCatalogsController API REST returns ResponseEntity of type ResponseBean

--- src/main/java/com/dulsystems/mta/controller/VehicleCatalogsController.java
+++ src/main/java/com/dulsystems/mta/controller/VehicleCatalogsController.java
@@ -1,7 +1,9 @@
 package com.dulsystems.mta.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,65 +27,65 @@ public class VehicleCatalogsController {
 	
 	//CONTROLLERS FOR VEHICLE LINE
 	@RequestMapping(value="getvehiclelinebyline/{line}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
-    public ResponseBean searchVehicleLineByLine(@PathVariable("line") /*@Pattern(regexp = "^\\d{2}$", message="El formato del id del empleado es invalido")*/ String line)  {
-		return vehicleCatalogsService.searchVehicleLineByLine(line);
+    public ResponseEntity<ResponseBean> searchVehicleLineByLine(@PathVariable("line") String line)  {
+		return new ResponseEntity<ResponseBean>(vehicleCatalogsService.searchVehicleLineByLine(line),HttpStatus.OK);
     }
 	
 	@RequestMapping(value="savenewvehicleline", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
-    public ResponseBean executeSaveVehicleLine(@Valid @RequestBody RequestBean request) {
-    	return vehicleCatalogsService.executeSaveVehicleLine(request);
+    public ResponseEntity<ResponseBean> executeSaveVehicleLine(@Valid @RequestBody RequestBean request) {
+		return new ResponseEntity<ResponseBean>(vehicleCatalogsService.executeSaveVehicleLine(request),HttpStatus.CREATED);
     }
 	
 	@RequestMapping(value="editvehicleline", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.PUT)
-    public ResponseBean executeUpdateVehicleLineByLine(@Valid @RequestBody RequestBean request) {
-    	return vehicleCatalogsService.executeUpdateVehicleLineByLine(request);
+    public ResponseEntity<ResponseBean> executeUpdateVehicleLineByLine(@Valid @RequestBody RequestBean request) {
+		return new ResponseEntity<ResponseBean>(vehicleCatalogsService.executeUpdateVehicleLineByLine(request),HttpStatus.OK);
     }
 	
 	@RequestMapping(value="removevehiclelinebyline/{line}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.DELETE)
-    public ResponseBean removeVehicleLineByLine(@PathVariable("line") /*@Pattern(regexp = "^\\d{2}$", message="El formato del id del empleado es invalido")*/ String line) {
-    	return vehicleCatalogsService.removeVehicleLineByLine(line);
+    public ResponseEntity<ResponseBean> removeVehicleLineByLine(@PathVariable("line") /*@Pattern(regexp = "^\\d{2}$", message="El formato del id del empleado es invalido")*/ String line) {
+		return new ResponseEntity<ResponseBean>(vehicleCatalogsService.removeVehicleLineByLine(line),HttpStatus.OK);
     }
 	
 	//CONTROLLERS FOR VEHICLE MODEL
 	@RequestMapping(value="getvehiclemodelbymodel/{model}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
-    public ResponseBean searchVehicleModelByModel(@PathVariable("model") /*@Pattern(regexp = "^\\d{2}$", message="El formato del id del empleado es invalido")*/ String model)  {
-		return vehicleCatalogsService.searchVehicleModelByModel(model);
+    public ResponseEntity<ResponseBean> searchVehicleModelByModel(@PathVariable("model") String model)  {
+		return new ResponseEntity<ResponseBean>(vehicleCatalogsService.searchVehicleModelByModel(model),HttpStatus.OK);
     }
 	
 	@RequestMapping(value="savenewvehiclemodel", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
-    public ResponseBean executeSaveVehicleModel(@Valid @RequestBody RequestBean request) {
-    	return vehicleCatalogsService.executeSaveVehicleModel(request);
+    public ResponseEntity<ResponseBean> executeSaveVehicleModel(@Valid @RequestBody RequestBean request) {
+		return new ResponseEntity<ResponseBean>(vehicleCatalogsService.executeSaveVehicleModel(request),HttpStatus.CREATED);
     }
 	
 	@RequestMapping(value="editvehiclemodel", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.PUT)
-    public ResponseBean executeUpdateVehicleModelByModel(@Valid @RequestBody RequestBean request) {
-    	return vehicleCatalogsService.executeUpdateVehicleModelByModel(request);
+    public ResponseEntity<ResponseBean> executeUpdateVehicleModelByModel(@Valid @RequestBody RequestBean request) {
+		return new ResponseEntity<ResponseBean>(vehicleCatalogsService.executeUpdateVehicleModelByModel(request),HttpStatus.OK);
     }
 	
 	@RequestMapping(value="removevehiclemodelbymodel/{model}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.DELETE)
-    public ResponseBean removeVehicleModelByModel(@PathVariable("model") /*@Pattern(regexp = "^\\d{2}$", message="El formato del id del empleado es invalido")*/ String model) {
-    	return vehicleCatalogsService.removeVehicleModelByModel(model);
+    public ResponseEntity<ResponseBean> removeVehicleModelByModel(@PathVariable("model") String model) {
+		return new ResponseEntity<ResponseBean>(vehicleCatalogsService.removeVehicleModelByModel(model),HttpStatus.OK);
     }
 	
 	//CONTROLLERS FOR VEHICLE YEAR
 	@RequestMapping(value="getvehicleyearbyyear/{year}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
-    public ResponseBean searchVehicleYearByYear(@PathVariable("year") /*@Pattern(regexp = "^\\d{2}$", message="El formato del id del empleado es invalido")*/ String year)  {
-		return vehicleCatalogsService.searchVehicleYearByYear(Integer.parseInt(year));
+    public ResponseEntity<ResponseBean> searchVehicleYearByYear(@PathVariable("year") String year)  {
+		return new ResponseEntity<ResponseBean>(vehicleCatalogsService.searchVehicleYearByYear(Integer.parseInt(year)),HttpStatus.OK);
     }
 	
 	@RequestMapping(value="savenewvehicleyear", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
-    public ResponseBean executeSaveVehicleYear(@Valid @RequestBody RequestBean rb) {
-    	return vehicleCatalogsService.executeSaveVehicleYear(rb);
+    public ResponseEntity<ResponseBean> executeSaveVehicleYear(@Valid @RequestBody RequestBean rb) {
+		return new ResponseEntity<ResponseBean>(vehicleCatalogsService.executeSaveVehicleYear(rb),HttpStatus.CREATED);
     }
 	
 	@RequestMapping(value="editvehicleyear", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.PUT)
-    public ResponseBean executeUpdateVehicleYearByYear(@Valid @RequestBody RequestBean rb) {
-    	return vehicleCatalogsService.executeUpdateVehicleYearByYear(rb);
+    public ResponseEntity<ResponseBean> executeUpdateVehicleYearByYear(@Valid @RequestBody RequestBean rb) {
+		return new ResponseEntity<ResponseBean>(vehicleCatalogsService.executeUpdateVehicleYearByYear(rb),HttpStatus.OK);
     }
 	
 	@RequestMapping(value="removevehicleyearbyyear/{year}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.DELETE)
-    public ResponseBean removeVehicleYearByYear(@PathVariable("year") /*@Pattern(regexp = "^\\d{2}$", message="El formato del id del empleado es invalido")*/ String year) {
-    	return vehicleCatalogsService.removeVehicleYearByYear(Integer.parseInt(year));
+    public ResponseEntity<ResponseBean> removeVehicleYearByYear(@PathVariable("year") String year) {
+		return new ResponseEntity<ResponseBean>(vehicleCatalogsService.removeVehicleYearByYear(Integer.parseInt(year)),HttpStatus.OK);
     }
 	
 }
